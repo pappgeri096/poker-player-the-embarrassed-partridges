f29c44e141f4546b4cb73273803fe45532a90444
stefanrakic001 <stefan.rakic001@gmail.com>
2018-09-24 12:16:53 +0200

    names

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,6 +1,7 @@
 package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.util.Map;
 
@@ -9,9 +10,22 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
+       JsonElement name = request.getAsJsonObject().get("players");
+        System.err.println(name);
         return 0;
     }
 
     public static void showdown(JsonElement game) {
     }
+
+    public static void main(String[] args) {
+        JsonElement jsonElement = new JsonObject();
+        betRequest(jsonElement);
+
+
+
+    }
+
+
+
 }
