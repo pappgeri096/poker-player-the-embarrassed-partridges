a9d573887e005fbcd4a917c34be91f98e2a318b6
Andras Fulop <andrasfulop86@gmail.com>
2018-09-24 11:18:38 +0200

    V0.1

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -6,10 +6,10 @@ import java.util.Map;
 
 public class Player {
 
-    static final String VERSION = "Default Java folding player";
+    static final String VERSION = "Java player V0.1";
 
     public static int betRequest(JsonElement request) {
-        return 0;
+        return 25;
     }
 
     public static void showdown(JsonElement game) {
