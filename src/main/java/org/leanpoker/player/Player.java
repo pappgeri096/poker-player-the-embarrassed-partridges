f5df177878701875360adfb7361bbab94795b1c3
stefanrakic001 <stefan.rakic001@gmail.com>
2018-09-24 14:07:02 +0200

    sdsd

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -11,11 +11,11 @@ public class Player {
 
     public static int betRequest(JsonElement request) {
         JsonElement names = request.getAsJsonObject().getAsJsonArray("players");
-        JsonElement round = request.getAsJsonObject();
+        JsonElement round = request.getAsJsonObject().getAsJsonPrimitive("round");
         JsonElement cards = request.getAsJsonObject().getAsJsonArray("players").get(3).getAsJsonObject().get("hole_cards");
         System.err.println("NAMES              "+ names);
         System.err.println("CARDS :            "+ cards);
-        System.err.println("OBJECKT :            "+ round);
+        System.err.println("ROUND :            "+ round);
 
         return 0;
     }
