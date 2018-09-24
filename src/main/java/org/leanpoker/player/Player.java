450615ab8076e3010367eb2334004a40ae36686f
pappgeri096 <pappgergo096@gmail.com>
2018-09-24 15:16:49 +0200

    Java player V0.1

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -16,18 +16,16 @@ public class Player {
         JsonElement betIndex = request.getAsJsonObject().getAsJsonPrimitive("bet_index");
         JsonElement cards = request.getAsJsonObject().getAsJsonArray("players").get(3).getAsJsonObject().get("hole_cards");
 
-        /**for(Map.Entry<String, JsonElement> entry : cards.getAsJsonObject().entrySet()) {
-            System.err.println("Key = " + entry.getKey() + " Value = " + entry.getValue() );
-        }*/
-
-
+//        for(Map.Entry<String, JsonElement> entry : cards.getAsJsonObject().entrySet()) {
+//            System.err.println("Key = " + entry.getKey() + " Value = " + entry.getValue() );
+//        }
 
         System.err.println("NAMES              "+ names);
         System.err.println("CARDS :            "+ cards);
         System.err.println("ROUND :            "+ round);
         System.err.println("BET INDEX: "+ betIndex);
 
-        return 300;
+        return 0;
     }
 
     public static void showdown(JsonElement game) {
