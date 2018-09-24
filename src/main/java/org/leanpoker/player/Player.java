4a230e11806e4a5da12faed33b9fd15006814d7e
Andras Fulop <andrasfulop86@gmail.com>
2018-09-24 14:21:10 +0200

    V0.5 bet index

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -7,15 +7,17 @@ import java.util.Map;
 
 public class Player {
 
-    static final String VERSION = "Default Java folding player";
+    static final String VERSION = "Java player V0.5";
 
     public static int betRequest(JsonElement request) {
         JsonElement names = request.getAsJsonObject().getAsJsonArray("players");
         JsonElement round = request.getAsJsonObject().getAsJsonPrimitive("round");
+        JsonElement betIndex = request.getAsJsonObject().getAsJsonPrimitive("bet_index");
         JsonElement cards = request.getAsJsonObject().getAsJsonArray("players").get(3).getAsJsonObject().get("hole_cards");
         System.err.println("NAMES              "+ names);
         System.err.println("CARDS :            "+ cards);
         System.err.println("ROUND :            "+ round);
+        System.err.println("BET INDEX: "+ betIndex);
 
         return 0;
     }
