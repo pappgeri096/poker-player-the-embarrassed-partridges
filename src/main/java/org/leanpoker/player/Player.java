6e40b4e083c5a7b7a7601b760c202b2fbcfb040c
Andras Fulop <andrasfulop86@gmail.com>
2018-09-24 13:49:49 +0200

    V0.3 Printing community cards

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -13,9 +13,11 @@ public class Player {
 
     public static int betRequest(JsonElement request) {
         JsonElement names = request.getAsJsonObject().getAsJsonArray("players");
+        JsonElement communityCards = request.getAsJsonObject().getAsJsonArray("community_cards");
         JsonElement cards = request.getAsJsonObject().getAsJsonArray("players").get(0).getAsJsonObject().get("hole_cards");
         System.err.println("NAMES              "+ names);
         System.err.println("CARDS :            "+ cards);
+        System.err.println("Community Cards: " + communityCards);
 
         return 0;
     }
