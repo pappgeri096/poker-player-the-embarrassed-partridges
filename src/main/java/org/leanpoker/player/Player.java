16cb6cdcd9d9a332d378fc0ade4a0d44182d9a2a
Andras Fulop <andrasfulop86@gmail.com>
2018-09-24 15:05:15 +0200

    V0.5 bet index

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -14,18 +14,20 @@ public class Player {
         JsonElement names = request.getAsJsonObject().getAsJsonArray("players");
         JsonElement round = request.getAsJsonObject().getAsJsonPrimitive("round");
         JsonElement betIndex = request.getAsJsonObject().getAsJsonPrimitive("bet_index");
-        JsonElement cards = request.getAsJsonObject().getAsJsonArray("players").get(3).getAsJsonObject().get("hole_cards");
+        JsonElement cards = request.getAsJsonObject().getAsJsonArray("players").get(3).getAsJsonArray().get("hole_cards");
 
-        for(Map.Entry<String, JsonElement> entry : cards.getAsJsonObject().entrySet()) {
+        /**for(Map.Entry<String, JsonElement> entry : cards.getAsJsonObject().entrySet()) {
             System.err.println("Key = " + entry.getKey() + " Value = " + entry.getValue() );
-        }
+        }*/
+
+
 
         System.err.println("NAMES              "+ names);
         System.err.println("CARDS :            "+ cards);
         System.err.println("ROUND :            "+ round);
         System.err.println("BET INDEX: "+ betIndex);
 
-        return 0;
+        return 300;
     }
 
     public static void showdown(JsonElement game) {
